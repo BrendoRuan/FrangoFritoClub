b2a39f94f5097a0b18b892c3c1889de3344d7608
BrendoRuan <rhuanoliveira001@gmail.com>
2024-06-05 15:34:27 -0300

    atualizando

--- src/main/java/com/FrangoFrito/FrangoFrito/Entity/Categoria.java
+++ src/main/java/com/FrangoFrito/FrangoFrito/Entity/Categoria.java
@@ -7,11 +7,77 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.BeanUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
 public class Categoria {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    private String nomeCategoria;
+
+    @NotNull
+    private boolean statusCategoria;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Produto> produtos = new ArrayList<>();
+
+    public Categoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public Categoria() {}
+
+    public Categoria(CategoriaDTO categoria) {
+        BeanUtils.copyProperties(categoria, this);
+    }
+
+    public Integer getIdCategoria() {
+        return getIdCategoria();
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public boolean isStatusCategoria() {
+        return statusCategoria;
+    }
+
+    public void setStatusCategoria(boolean statusCategoria) {
+        this.statusCategoria = statusCategoria;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public void addProduto(Produto produto) {
+        produtos.add(produto);
+        produto.setCategoria(this);
+    }
+
+    public void removeProduto(Produto produto) {
+        produtos.remove(produto);
+        produto.setCategoria(null);
+    }
+   /* @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     @NotNull
@@ -20,9 +86,9 @@ public class Categoria {
     @NotNull
     private boolean statusCategoria;
 
-    @OneToMany(mappedBy = "Categoria",fetch = FetchType.LAZY,cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonIgnore
-    private List<Produto> produtos = new ArrayList<Produto>();
+    @OneToMany(mappedBy = "categoria"/*,cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Produto> produto = new ArrayList<Produto>();
+
     public Categoria(String nomeCategoria) {
         this.nomeCategoria = nomeCategoria;
     }
@@ -47,20 +113,20 @@ public class Categoria {
         this.nomeCategoria = nomeCategoria;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+    public List<Produto> getProduto() {
+        return produto;
     }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
+    public void setProduto(List<Produto> produto) {
+        this.produto = produto;
     }
     public void addProduto(Produto produto) {
-        produtos.add(produto);
+      //  produto.add(produto);
         produto.setCategoria(this);
     }
 
     public void removeProduto(Produto produto) {
-        produtos.remove(produto);
+     //   produto.remove(produto);
         produto.setCategoria(null);
     }
 
@@ -70,5 +136,5 @@ public class Categoria {
 
     public void setStatusCategoria(boolean statusCategoria) {
         this.statusCategoria = statusCategoria;
-    }
+    }*/
 }
--- src/main/java/com/FrangoFrito/FrangoFrito/Entity/Produto.java
+++ src/main/java/com/FrangoFrito/FrangoFrito/Entity/Produto.java
@@ -10,6 +10,116 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @NotNull
+    private String nomeProduto;
+
+    @NotNull
+    private Double valorDeCusto;
+
+    @NotNull
+    private Double valorDeVenda;
+
+    @NotNull
+    private boolean statusProduto;
+
+    @NotNull
+    private boolean produtoEmDestaque;
+
+    @NotNull
+    private String descricao;
+
+    private String imagem;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Produto(String nomeProduto, String imagem, Double valorDeCusto, Double valorDeVenda, Categoria categoria, String descricao) {
+        this.nomeProduto = nomeProduto;
+        this.valorDeCusto = valorDeCusto;
+        this.valorDeVenda = valorDeVenda;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.imagem = imagem;
+    }
+
+    public Produto() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public Double getValorDeCusto() {
+        return valorDeCusto;
+    }
+
+    public void setValorDeCusto(Double valorDeCusto) {
+        this.valorDeCusto = valorDeCusto;
+    }
+
+    public Double getValorDeVenda() {
+        return valorDeVenda;
+    }
+
+    public void setValorDeVenda(Double valorDeVenda) {
+        this.valorDeVenda = valorDeVenda;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isStatusProduto() {
+        return statusProduto;
+    }
+
+    public void setStatusProduto(boolean statusProduto) {
+        this.statusProduto = statusProduto;
+    }
+
+    public boolean isProdutoEmDestaque() {
+        return produtoEmDestaque;
+    }
+
+    public void setProdutoEmDestaque(boolean produtoEmDestaque) {
+        this.produtoEmDestaque = produtoEmDestaque;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+    /*@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     @NotNull
     private String nomeProduto;
     @NotNull
@@ -28,7 +138,7 @@ public class Produto {
     "id":1
 }
 }
-    * */
+
     @NotNull
     private boolean statusProduto;
     @NotNull
@@ -40,8 +150,8 @@ public class Produto {
 
 
     //@NotNull
-    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JoinColumn(name = "categoria_id")
+    @ManyToOne
+    @JoinColumn(name = "categoria")
     private Categoria categoria;
 
     public Produto(String nomeProduto, String imagem, Double valorDeCusto, Double valorDeVenda, Categoria categoria,String descricao) {
@@ -123,5 +233,5 @@ public class Produto {
 
     public void setImagem(String imagem) {
         this.imagem = imagem;
-    }
+    }*/
 }
